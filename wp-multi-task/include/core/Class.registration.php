cf91ca2214d504060a79326d7f5d266f21e7ccf8
Abu Jafar Md. Salah <naharsoftbd@gmail.com>
2017-05-30 13:38:05 +0600

    registration bug fix

--- wp-multi-task/include/core/Class.registration.php
+++ wp-multi-task/include/core/Class.registration.php
@@ -45,32 +45,31 @@
             $user_id = email_exists($user_email);
         }
         if ( !$user_id and email_exists($user_email) == false ) {
-                $random_password = wp_generate_password( $length=12, $include_standard_special_chars=false );
+                    $random_password = wp_generate_password( $length=12, $include_standard_special_chars=false );
 					$user_id = wp_create_user( $user_name, $random_password, $user_email );
                     $user = new WP_User($user_id);
  
                     $user_login = stripslashes($user->user_login);
                     $user_email = stripslashes($user->user_email);
-
-                    $message  = sprintf(__('New user registration on your blog %s:'), get_option('blogname')) . "\n";
-                    $message .= sprintf(__('Username: %s'), $user_login) . "\n";
-                    $message .= sprintf(__('E-mail: %s'), $user_email) . "\n";
-                    $headers = array('Content-Type: text/html; charset=UTF-8','From: '.get_option('blogname').' <info@nsbd.net');
-                    @wp_mail(get_option('admin_email'), sprintf(__('[%s] New User Registration'), get_option('blogname')), $message,$headers);
-
-                    if ( empty($random_password) )
+                    
+                    
+                    if (empty($random_password) )
                         return;
 
-                    $message  = __('Hi there,') . "\n";
+                    $message .= __('Hi there,') . "\n";
                     $message .= sprintf(__("Welcome to %s! Here's how to log in:"), get_option('blogname')) . "\n";
                     $message .= home_url('/login') . "\n";
                     $message .= sprintf(__('Username: %s'), $user_login) . "\n";
                     $message .= sprintf(__('Password: %s'), $random_password) . "\n";
                     $message .= sprintf(__('If you have any problems, please contact me at %s.'), get_option('admin_email')) . "\n";
                     
-
-                    wp_mail($user_email, sprintf(__('[%s] Your username and password'), get_option('blogname')), $message,$headers);
-		
+                    $to = $user_email;
+					$subject = sprintf(__('[%s] Your username and password'), get_option('blogname'));
+					$body = 'The email body content';
+					$headers = array('Content-Type: text/html; charset=UTF-8','From: '.get_option('blogname').' <'.get_option('admin_email').'>');
+					 
+					wp_mail( $to, $subject, $message, $headers );
+                   
                 echo '<div class="alert alert-success alert-dismissable fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>User '.__($user_name.' created successfully, please check your email inbox or junk or spam for getting password','istcoderbooking').'</div>';
         } else {
                 echo $random_password = '<div class="alert alert-danger alert-dismissable fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'.__('User already exists.  Password inherited.').'</div>';
